90d8b2a79f144c0ce4976de475e5f966226223b2
Ogabek <mustafoev0806@gmail.com>
2023-07-22 23:00:18 +0500

    rel icon added

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import { Nav, Provider } from "@components";
 import "@styles/globals.css";
+import Head from "next/head";
 
 export const metadata = {
   title: "Promptopia",
@@ -9,6 +10,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" type="image/svg+xml" href="./assets/images/logo.svg" />
+      </Head>
       <body>
         <Provider>
           <div className="main">
